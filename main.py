6a7c3a7301980795a5d2bf57819e16e9dc0e77a5
Konstantin Malanchev <hombit@gmail.com>
2023-10-04 09:28:13 -0400

    Run black first time

--- main.py
+++ main.py
@@ -11,27 +11,31 @@ import json
 
 app = FastAPI()
 
+
 class Observation(BaseModel):
     mjd: float
     flux: float
     fluxerr: float
     zp: float = 8.9
-    zpsys: Literal['ab', 'vega'] = 'ab'
+    zpsys: Literal["ab", "vega"] = "ab"
     band: str
-    
+
+
 class Target(BaseModel):
     light_curve: List[Observation]
     ebv: float
     t_min: float
     t_max: float
     count: int
-    name_model: str 
+    name_model: str
     redshift: List[float]
-    
+
+
 class Point(BaseModel):
     time: float
     flux: float
     band: str
+
 
 class Result(BaseModel):
     flux_jansky: List[Point]
@@ -39,38 +43,42 @@ class Result(BaseModel):
     covariance: List[List[float]]
     chi2: float
     parameters: Dict[str, float]
-    
-    
+
+
 def fit(data, name_model, ebv, redshift):
     dust = sncosmo.CCM89Dust()
-    model = sncosmo.Model(source=name_model, effects=[dust], effect_names=['mw'], effect_frames=['obs'])
+    model = sncosmo.Model(source=name_model, effects=[dust], effect_names=["mw"], effect_frames=["obs"])
     model.set(mwebv=ebv)
-    summary, fitted_model = sncosmo.fit_lc(data, model, model.param_names, bounds={'z':(redshift[0], redshift[1])})
+    summary, fitted_model = sncosmo.fit_lc(
+        data, model, model.param_names, bounds={"z": (redshift[0], redshift[1])}
+    )
     return summary, fitted_model
 
 
 def get_flux_and_params(summary, data, fitted_model, t_min, t_max, count):
-
     segment = np.linspace(t_min, t_max, count)
     df = data.to_pandas()
     points = []
-    for band in df['band'].unique():
-        predicts = fitted_model.bandflux(band, segment, df['zp'][0], df['zpsys'][0])
+    for band in df["band"].unique():
+        predicts = fitted_model.bandflux(band, segment, df["zp"][0], df["zpsys"][0])
         points += [Point(time=time, flux=flux, band=band) for time, flux in zip(segment, predicts)]
-    return Result(flux_jansky=points, parameters=dict(zip(summary.param_names, summary.parameters)), 
-                       degrees_of_freedom=summary.ndof, covariance=summary.covariance.tolist(), chi2=summary.chisq)
+    return Result(
+        flux_jansky=points,
+        parameters=dict(zip(summary.param_names, summary.parameters)),
+        degrees_of_freedom=summary.ndof,
+        covariance=summary.covariance.tolist(),
+        chi2=summary.chisq,
+    )
 
 
 def approximate(data: Target):
-
     df = pd.DataFrame([obs.dict() for obs in data.light_curve])
     table = Table.from_pandas(df)
     summary, fitted_model = fit(table, data.name_model, data.ebv, data.redshift)
     result = get_flux_and_params(summary, table, fitted_model, data.t_min, data.t_max, data.count)
     return result
-    
 
-@app.post('/api/v1/sncosmo')
+
+@app.post("/api/v1/sncosmo")
 async def sn_cosmo(data: Target):
-    
     return approximate(data)
